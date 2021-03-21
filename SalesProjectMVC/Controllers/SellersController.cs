1d490819db3b61296223751ba905fc14f81665f5
Sthevan Bello Alves <sthevan.alves@gmail.com>
2021-03-21 16:44:56 -0300

    Simple create form

--- SalesProjectMVC/Controllers/SellersController.cs
+++ SalesProjectMVC/Controllers/SellersController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SalesProjectMVC.Models;
 using SalesProjectMVC.Services;
 using System;
 using System.Collections.Generic;
@@ -23,6 +24,17 @@ namespace SalesProjectMVC.Controllers
             return View(list);
         }
 
+        public IActionResult Create()
+        {
+            return View();
+        }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerServices.Insert(seller);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
--- SalesProjectMVC/Services/SellerServices.cs
+++ SalesProjectMVC/Services/SellerServices.cs
@@ -20,5 +20,11 @@ namespace SalesProjectMVC.Services
         {
             return _context.Seller.ToList();
         }
+
+        public void Insert(Seller obj)
+        {
+            _context.Add(obj);
+            _context.SaveChanges();
+        }
     }
 }
