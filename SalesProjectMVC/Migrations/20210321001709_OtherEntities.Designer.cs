23143eb94791de08f9bb7c7241e71a472d5aabee
Sthevan Bello Alves <sthevan.alves@gmail.com>
2021-03-20 21:20:01 -0300

    Other entities and second migration

--- SalesProjectMVC/Migrations/20210321001709_OtherEntities.Designer.cs
+++ SalesProjectMVC/Migrations/20210321001709_OtherEntities.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SalesProjectMVC.Data;
 
 namespace SalesProjectMVC.Migrations
 {
     [DbContext(typeof(SalesProjectMVCContext))]
-    partial class SalesProjectMVCContextModelSnapshot : ModelSnapshot
+    [Migration("20210321001709_OtherEntities")]
+    partial class OtherEntities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ SalesProjectMVC/Models/SalesRecord.cs
@@ -0,0 +1,29 @@
+﻿using SalesProjectMVC.Models.Enums;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace SalesProjectMVC.Models
+{
+    public class SalesRecord
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public double Amount { get; set; }
+        public SaleStatus Status { get; set; }
+        public Seller Seller { get; set; }
+
+        public SalesRecord()
+        {
+        }
+
+        public SalesRecord(int id, DateTime date, double amount, SaleStatus status, Seller seller)
+        {
+            Id = id;
+            Date = date;
+            Amount = amount;
+            Status = status;
+            Seller = seller;
+        }
+    }
+}
