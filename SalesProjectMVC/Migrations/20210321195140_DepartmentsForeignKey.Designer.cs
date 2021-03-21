88c670c86485e9551b089b94eb1d3d22f681db52
Sthevan Bello Alves <sthevan.alves@gmail.com>
2021-03-21 16:58:43 -0300

    Foreign key not null (referential integrity)

--- SalesProjectMVC/Migrations/20210321195140_DepartmentsForeignKey.Designer.cs
+++ SalesProjectMVC/Migrations/20210321195140_DepartmentsForeignKey.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SalesProjectMVC.Data;
 
 namespace SalesProjectMVC.Migrations
 {
     [DbContext(typeof(SalesProjectMVCContext))]
-    partial class SalesProjectMVCContextModelSnapshot : ModelSnapshot
+    [Migration("20210321195140_DepartmentsForeignKey")]
+    partial class DepartmentsForeignKey
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- SalesProjectMVC/Services/SellerServices.cs
+++ SalesProjectMVC/Services/SellerServices.cs
@@ -23,6 +23,7 @@ namespace SalesProjectMVC.Services
 
         public void Insert(Seller obj)
         {
+            obj.Department = _context.Department.First();
             _context.Add(obj);
             _context.SaveChanges();
         }
